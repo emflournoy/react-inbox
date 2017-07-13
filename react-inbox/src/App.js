a6f84e570ca6c72f0c68658d440828510665fd1d
Elizabeth Flournoy <bflournoy@gmail.com>
2017-07-13 16:48:55 -0600

    inital commit

---
+++ react-inbox/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Toolbar from './components/Toolbar';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Toolbar />
+      </div>
+    );
+  }
+}
+
+export default App;
